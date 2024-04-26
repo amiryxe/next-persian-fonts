eca714367f6725f0b09f7a0dbab8bc24f75db79f
Amir Salehi <nik.amirsa@gmail.com>
2024-04-26 21:21:52 +0330

    add fonts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,38 @@
-import Image from 'next/image'
-import Link from 'next/link'
+import { sahel } from '@/next-persian-fonts'
 
 export default function Home() {
-  return <div>Next Persian Fonts</div>
+  return (
+    <div className={`container py-8 flex flex-col gap-8 ${sahel.className}`} dir="rtl">
+      <div>
+        <h1 className="font-sans font-light text-4xl">Next Persian Fonts</h1>
+        <h2 className="font-bold text-2xl">پکیج مجموعه فونت‌های فارسی برای Next.js</h2>
+      </div>
+
+      <article>
+        <p className="mt-8">در حال حاضر فونت‌های زیر در این پکیج پشتیبانی می‌شوند:</p>
+
+        <ul className="list-disc pr-8">
+          <li>فونت ساحل</li>
+          <li>فونت وزیر</li>
+        </ul>
+      </article>
+
+      <section>
+        <h2>نصب:</h2>
+
+        <code>npm install next-persian-fonts</code>
+      </section>
+
+      <section>
+        <h2>استفاده:</h2>
+
+        <code>
+          {/* import {sahel} from '@/next-persian-fonts'
+            <div
+              className={`container py-8 flex flex-col gap-8 ${sahel.className}`}
+            > */}
+        </code>
+      </section>
+    </div>
+  )
 }
---
+++ src/next-persian-fonts/index.ts
@@ -0,0 +1,53 @@
+import localFont from 'next/font/local'
+
+export const vazirMatn = localFont({
+  src: [
+    {
+      path: './assets/fonts/vazirmatn/Vazirmatn-FD-Thin.woff2',
+      weight: '100',
+      style: 'normal',
+    },
+    {
+      path: './assets/fonts/vazirmatn/Vazirmatn-FD-ExtraLight.woff2',
+      weight: '200',
+      style: 'normal',
+    },
+    {
+      path: './assets/fonts/vazirmatn/Vazirmatn-FD-Light.woff2',
+      weight: '300',
+      style: 'normal',
+    },
+    {
+      path: './assets/fonts/vazirmatn/Vazirmatn-FD-Regular.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: './assets/fonts/vazirmatn/Vazirmatn-FD-Medium.woff2',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: './assets/fonts/vazirmatn/Vazirmatn-FD-SemiBold.woff2',
+      weight: '600',
+      style: 'normal',
+    },
+    {
+      path: './assets/fonts/vazirmatn/Vazirmatn-FD-Bold.woff2',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: './assets/fonts/vazirmatn/Vazirmatn-FD-ExtraBold.woff2',
+      weight: '800',
+      style: 'normal',
+    },
+    {
+      path: './assets/fonts/vazirmatn/Vazirmatn-FD-Black.woff2',
+      weight: '900',
+      style: 'normal',
+    },
+  ],
+})
+
+export const sahel = localFont({ src: './assets/fonts/sahel/Sahel-VF.woff2' })
