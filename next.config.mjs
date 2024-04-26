abea2c51de2aeca2249dfb311e9abd37b05bb85b
Amir Salehi <nik.amirsa@gmail.com>
2024-04-26 20:03:14 +0330

    update configs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     basePath: "/next-persian-fonts",
+    assetPrefix: '/next-persian-fonts',
     output: "export",
     reactStrictMode: true,
 };
