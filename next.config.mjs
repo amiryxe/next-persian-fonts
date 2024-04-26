7b977f3b0a19cfc5478d06e4a0bd6b444d8c9605
Amir Salehi <nik.amirsa@gmail.com>
2024-04-26 20:15:26 +0330

    remove some extra codes

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     basePath: "/next-persian-fonts",
-    assetPrefix: '/next-persian-fonts',
     output: "export",
     reactStrictMode: true,
 };
