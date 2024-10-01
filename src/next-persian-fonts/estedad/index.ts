385c45a40062765ca8ca2da1fdcda1147fa8ff10
Amir Salehi <nik.amirsa@gmail.com>
2024-10-01 12:29:52 +0330

    add estedad font

---
+++ src/next-persian-fonts/estedad/index.ts
@@ -0,0 +1,4 @@
+import localFont from 'next/font/local'
+
+export const estedad = localFont({ src: './Estedad[KSHD,wght].woff2' })
+export const estedadFD = localFont({ src: './Estedad-FD[KSHD,wght].woff2' })
