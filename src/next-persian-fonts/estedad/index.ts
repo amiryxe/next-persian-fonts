9a2aca8410d47d35941689fea7e804eda2fe7bd4
Amir Salehi <nik.amirsa@gmail.com>
2024-10-07 12:56:05 +0330

    fix: estedad variable font name

--- src/next-persian-fonts/estedad/index.ts
+++ src/next-persian-fonts/estedad/index.ts
@@ -1,8 +1,8 @@
 import localFont from 'next/font/local'
 
-export const estedad = localFont({ src: './Estedad[KSHD,wght].woff2', variable: '--font-dana' })
+export const estedad = localFont({ src: './Estedad[KSHD,wght].woff2', variable: '--font-estedad' })
 
 export const estedadFD = localFont({
   src: './Estedad-FD[KSHD,wght].woff2',
-  variable: '--font-dana-fd',
+  variable: '--font-estedad-fd',
 })
